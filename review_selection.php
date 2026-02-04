f746ad6c61eb26db31a5b0974996cc50e21a7ca7
nicole849 <lucynguzo26@gmail.com>
2026-02-04 12:55:11 +0300

    .

--- review_selection.php
+++ review_selection.php
@@ -4,15 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet"href="style.css">
 </head>
 <body>
     <section>
+ <div class="review-container">
     <h1>Review Your Selection</h1>
     <h3>Please review your choices before submitting your vote</h3>
-    <div>
+    <div class="notice">
         <h>Important Notice</h>
-        <p>Once you submiot your vote,it caanot be changed.Please ensure all your selections are correct before proceeding</p>
+        <p>Once you submit your vote,it caanot be changed.Please ensure all your selections are correct before proceeding</p>
     </div>
+ </div>
     </section>
 </body>
 </html>
